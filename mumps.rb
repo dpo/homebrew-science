dc616c9158bc8172cff05642f4906b797bd68340
Dominique Orban <dominique.orban@gmail.com>
2015-09-12 21:37:09 -0400

    Trigger a build.

--- mumps.rb
+++ mumps.rb
@@ -3,7 +3,7 @@ class Mumps < Formula
   url "http://mumps.enseeiht.fr/MUMPS_4.10.0.tar.gz"
   mirror "http://graal.ens-lyon.fr/MUMPS/MUMPS_4.10.0.tar.gz"
   sha1 "904b1d816272d99f1f53913cbd4789a5be1838f7"
-  revision 2
+  revision 3
 
   bottle do
     root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
