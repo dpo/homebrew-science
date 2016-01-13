be21b42e0be90867115f07ae00cc90c845147287
Dominique Orban <dominique.orban@gmail.com>
2016-01-13 17:59:10 -0500

    plasma: new formula.

--- plasma.rb
+++ plasma.rb
@@ -1,5 +1,5 @@
 class Plasma < Formula
-  desc "Parallel Linear Algebra Software for Multicore Architectures"
+  desc "Parallel Linear Algebra for Multicore Architectures"
   homepage "http://icl.cs.utk.edu/plasma"
   url "http://icl.cs.utk.edu/projectsfiles/plasma/pubs/plasma_2.8.0.tar.gz"
   sha256 "e8758a71ddd02ad1fb57373cfd62fb1b32cebea62ba517484f1adf9f0afb1ddb"
@@ -8,43 +8,73 @@ class Plasma < Formula
   depends_on :fortran
 
   resource "lapacke" do
-    url "http://icl.cs.utk.edu/projectsfiles/plasma/pubs/lapacke.tgz"
-    sha256 "506344e126cf774ff9b7180104f87a580fa938c499179d8747cd97e4d4a543d3"
+    # LAPACKE is now included in the main LAPACK distribution.
+    url "http://www.netlib.org/lapack/lapack-3.6.0.tgz"
+    sha256 "a9a0082c918fe14e377bbd570057616768dca76cbdc713457d8199aaa233ffc3"
   end
 
   def install
     resource("lapacke").stage do
-      system "make", "CC=#{ENV.cc}", "LINKER=#{ENV["FC"]}", "LIBS=-llapack -lblas -lm", "RANLIB=true"
-      include.install Dir["include/*"]
-      lib.install "lapacke.a"
+      cp "make.inc.example", "make.inc"
+      make_args = [
+        "CC=#{ENV.cc}",
+        "FORTRAN=#{ENV["FC"]}",
+        "LOADER=#{ENV["FC"]}",
+        "RANLIB=true",
+      ]
+      cd "LAPACKE" do
+        system "make", "lapacke", *make_args
+      end
+      system "make", "tmglib", *make_args
+      include.install Dir["LAPACKE/include/*.h"]
+      lib.install "liblapacke.a", "libtmglib.a"
     end
 
-    # ENV.deparallelize
-    make_args = %W[
-      prefix=#{prefix}
-      CC=#{ENV.cc}
-      FC=#{ENV["FC"]}
-      RANLIB=true
-      FFLAGS=#{ENV["FFLAGS"]}
-      LIBBLAS=-lblas
-      LIBLAPACK=-llapack
-      LIBCBLAS=-lcblas
-      INCCLAPACK=-I#{include}
-      LIBCLAPACK=#{lib}/lapacke.a
-      PLASMA_F90=1
+    make_args = [
+      "prefix=#{prefix}",
+      "CC=#{ENV.cc}",
+      "FC=#{ENV["FC"]}",
+      "RANLIB=true",
+      "FFLAGS=#{ENV["FFLAGS"]}",
+      "LIBBLAS=-lblas",
+      "LIBLAPACK=-llapack",
+      "LIBCBLAS=-lcblas",
+      "INCCLAPACK=-I#{include}",
+      "LIBCLAPACK=-L#{lib} -llapacke -ltmglib",
+      "PLASMA_F90=1",
     ]
 
     cp "makes/make.inc.mac", "make.inc"
     system "make", *make_args
-    system "false"
+    system "make", "test"
+    system "make", "example"
+    lib.install Dir["lib/*"], "quark/libquark.a"
+    include.install Dir["include/*"], Dir["quark/*.h"]
+    doc.install Dir["docs/pdf/*"]
+    pkgshare.install Dir["examples/*.c"], Dir["examples/*.f"], Dir["examples/*.f90"], Dir["examples/*.d"]
+    pkgshare.install "timing"
   end
 
   def caveats; <<-EOS.undent
-    export VECLIB_MAXIMUM_THREADS=1
+    PLASMA should not be used in conjunction with a multithreaded BLAS to avoid
+    creating more threads than actual cores. Please set
+
+      export VECLIB_MAXIMUM_THREADS=1
+
+    when using PLASMA.
     EOS
   end
 
   test do
-    system "false"
+    ENV.fortran
+    ENV["VECLIB_MAXIMUM_THREADS"] = "1"
+    cp pkgshare/"example_dposv_f.f", testpath
+    libs = [
+      "-L#{opt_lib}", "-lplasma", "-lcoreblasqw", "-lcoreblas", "-lquark",
+      "-L#{Formula["hwloc"].opt_lib}", "-lhwloc",
+      "-lblas", "-llapack", "-llapacke"
+    ]
+    system ENV["FC"], "example_dposv_f.f", "-I#{opt_include}", "-o", "example_dposv_f", *libs
+    system "./example_dposv_f"
   end
 end
