2668609048d3a6e088c54d20c474034c5ecb24fc
Dominique Orban <dominique.orban@gmail.com>
2016-10-23 11:50:01 -0400

    mumps: fix blas dep

--- mumps.rb
+++ mumps.rb
@@ -4,7 +4,7 @@ class Mumps < Formula
   url "http://mumps.enseeiht.fr/MUMPS_5.0.1.tar.gz"
   mirror "http://graal.ens-lyon.fr/MUMPS/MUMPS_5.0.1.tar.gz"
   sha256 "50355b2e67873e2239b4998a46f2bbf83f70cdad6517730ab287ae3aae9340a0"
-  revision 4
+  revision 5
 
   bottle do
     cellar :any
@@ -14,6 +14,12 @@ class Mumps < Formula
   end
 
   depends_on :mpi => [:cc, :cxx, :f90, :recommended]
+  if OS.mac?
+    depends_on "openblas" => :optional
+    depends_on "veclibfort" if build.without?("openblas")
+  else
+    depends_on "openblas"
+  end
   if build.with? "mpi"
     depends_on "scalapack" => (build.with? "openblas") ? ["with-openblas"] : []
   end
@@ -21,9 +27,6 @@ class Mumps < Formula
   depends_on "parmetis" => :optional if build.with? "mpi"
   depends_on "scotch5"  => :optional
   depends_on "scotch"   => :optional
-
-  depends_on "openblas" => :optional
-  depends_on "veclibfort" if build.without?("openblas") && OS.mac?
 
   depends_on :fortran
 
